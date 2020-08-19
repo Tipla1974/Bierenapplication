bca5dd99a9b62d268c83538db50994b79aa2e521
Tiani Platteau <tiani.platteau@telenet.be>
2020-08-19 09:40:29 +0200

    Add project files.

---
+++ Bierenapplication/Controllers/BierController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bierenapplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bierenapplication.Controllers
+{
+    public class BierController : Controller
+    {
+        public IActionResult Index()
+        {
+            var bieren = new List<Bier>();
+            bieren.Add(new Bier {
+                ID = 22,
+                Naam = "Kruger",
+                Alcohol = 4.8F
+            }) ;
+            bieren.Add(new Bier
+            {
+                ID = 23,
+                Naam = "Duvel",
+                Alcohol = 8F
+            });
+            bieren.Add(new Bier
+            {
+                ID = 40,
+                Naam = "Jupiler",
+                Alcohol = 5.2F
+            });
+            return View(bieren);
+        }
+    }
+}
---
+++ Bierenapplication/Models/Bier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bierenapplication.Models
+{
+    public class Bier
+    {
+
+        public int ID { get; set; }
+
+        public string Naam { get; set; }
+
+        public float Alcohol { get; set; }
+    }
+}
